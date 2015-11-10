bf18a2979c287e9c269693dff017235211fcdf30
Dara <daraclare@gmail.com>
2015-11-10 08:30:08 +0000

    fixed possible_answers, kept replies

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   
 resources :polls do
   resources :questions
+  resources :replies, only: [ :new, :create ]
 end
 
 get 'polls' => "polls#index"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151108122212) do
+ActiveRecord::Schema.define(version: 20151109211033) do
+
+  create_table "answers", force: :cascade do |t|
+    t.integer  "reply_id"
+    t.integer  "question_id"
+    t.integer  "possible_answer_id"
+    t.string   "value"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+  end
+
+  add_index "answers", ["possible_answer_id"], name: "index_answers_on_possible_answer_id"
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id"
+  add_index "answers", ["reply_id"], name: "index_answers_on_reply_id"
 
   create_table "polls", force: :cascade do |t|
     t.string   "title"
@@ -35,5 +48,13 @@ ActiveRecord::Schema.define(version: 20151108122212) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "replies", force: :cascade do |t|
+    t.integer  "poll_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "replies", ["poll_id"], name: "index_replies_on_poll_id"
 
 end
