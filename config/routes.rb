d1c24771321ab555fc14979279ffa81936c782c4
Dara <daraclare@gmail.com>
2015-11-16 21:57:14 +0000

    styling of polls and poll id page

--- config/routes.rb
+++ config/routes.rb
@@ -15,14 +15,16 @@ end
 
 
   get 'pages/home'
-
   get 'pages/about'
+  root 'pages#home'
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'pages#home'
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
